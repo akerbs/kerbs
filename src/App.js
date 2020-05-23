e28b25515ec2c83e739a0888d129a2f35311471f
Anatol Kerbs <anker2702@gmail.com>
2020-05-23 14:48:04 +0200

    pwa8

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import "./media.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Home from "./Components/Home";
-import Cv from "./Components/Cv";
 import Portfolio from "./Components/Portfolio";
 import Contact from "./Components/Contact";
 
@@ -16,7 +15,6 @@ function App() {
         <Header />
         <Route exact path="/" component={Home} />
         <Route path="/portfolio" component={Portfolio} />
-        <Route path="/cv" component={Cv} />
         <Route path="/contact" component={Contact} />
 
         <Footer />
