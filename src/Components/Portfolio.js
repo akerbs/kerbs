dc6629083d9d34b01f9adaf3a17bdcf0c29e7edd
Anatol Kerbs <anker2702@gmail.com>
2021-02-15 00:01:54 +0300

    150221

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -4,14 +4,92 @@ import item2 from "../img/item2.png";
 import item3 from "../img/item3.png";
 import item4 from "../img/item4.png";
 import item5 from "../img/item5.png";
-import item6 from "../img/item6.png";
+import item6 from "../img/item6.jpg";
+import item7 from "../img/item7.jpg";
+import item8 from "../img/item8.jpg";
 
 export default function Portfolio() {
   return (
     <div className="Portfolio-content">
       <div className="Portfolio-item">
         <div className="Item-img-container">
-          <img src={item6} alt="" className="Item-img" />
+          <img src={item8} alt="item8" className="Item-img" />
+        </div>
+        <div className="Item-text">
+          <span className="highlight-1">My Store (demo)</span>
+          <br />
+          <div>2020</div>
+          <br />
+          <span className="highlight-2">Technology</span>
+          <br />
+          <div>
+            HTML, CSS, JavaScript, React, Gatsby, JSS, Material UI, NodeJS,
+            Google Analytics
+          </div>
+          <br />
+          <span className="highlight-2">Goal</span>
+          <br />
+          <div>
+            Create responsive multilingual and multicurrency e-store using
+            Stripe API.
+          </div>
+          <br />
+          <br />
+          <a
+            href="https://my-store-demo.vercel.app/"
+            onClick="this.blur()"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Check it out
+          </a>
+          <br /> <br />
+          <br />
+          <br />
+        </div>
+      </div>
+
+      <div className="Portfolio-item">
+        <div className="Item-img-container">
+          <img src={item7} alt="item7" className="Item-img" />
+        </div>
+        <div className="Item-text">
+          <span className="highlight-1">Salvegeorgia.com (demo)</span>
+          <br />
+          <div>2020</div>
+          <br />
+          <span className="highlight-2">Technology</span>
+          <br />
+          <div>
+            HTML, CSS, JavaScript, React, Gatsby, JSS, Material UI, NodeJS,
+            Google Analytics
+          </div>
+          <br />
+          <span className="highlight-2">Goal</span>
+          <br />
+          <div>
+            Create a responsive multilingual website of international medical,
+            legal and personnel services.
+          </div>
+          <br />
+          <br />
+          <a
+            href="https://salvegeorgia-demo.vercel.app/"
+            onClick="this.blur()"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Check it out
+          </a>
+          <br /> <br />
+          <br />
+          <br />
+        </div>
+      </div>
+
+      <div className="Portfolio-item">
+        <div className="Item-img-container">
+          <img src={item6} alt="item6" className="Item-img" />
         </div>
         <div className="Item-text">
           <span className="highlight-1">Suliko Restaurant</span>
@@ -21,14 +99,15 @@ export default function Portfolio() {
           <span className="highlight-2">Technology</span>
           <br />
           <div>
-            HTML, CSS, JavaScript, React, JSS, Material UI, NodeJS, Google
-            Analytics
+            HTML, CSS, JavaScript, React, Gatsby, JSS, Material UI, NodeJS,
+            Google Analytics
           </div>
           <br />
           <span className="highlight-2">Goal</span>
           <br />
           <div>
-            Create a multilingual restaurant website with table reservations.
+            Create a responsive multilingual restaurant website with table
+            reservations.
           </div>
           <br />
           <br />
@@ -48,7 +127,7 @@ export default function Portfolio() {
 
       <div className="Portfolio-item">
         <div className="Item-img-container">
-          <img src={item5} alt="" className="Item-img" />
+          <img src={item5} alt="item5" className="Item-img" />
         </div>
         <div className="Item-text">
           <span className="highlight-1">URL Shortener</span>
@@ -83,7 +162,7 @@ export default function Portfolio() {
 
       <div className="Portfolio-item">
         <div className="Item-img-container">
-          <img src={item4} alt="" className="Item-img" />
+          <img src={item4} alt="item4" className="Item-img" />
         </div>
         <div className="Item-text">
           <span className="highlight-1">Store - Admin area</span>
@@ -121,7 +200,7 @@ export default function Portfolio() {
 
       <div className="Portfolio-item">
         <div className="Item-img-container">
-          <img src={item3} alt="" className="Item-img" />
+          <img src={item3} alt="item3" className="Item-img" />
         </div>
         <div className="Item-text">
           <span className="highlight-1">Store SPA</span>
@@ -158,7 +237,7 @@ export default function Portfolio() {
 
       <div className="Portfolio-item">
         <div className="Item-img-container">
-          <img src={item2} alt="" className="Item-img" />
+          <img src={item2} alt="item2" className="Item-img" />
         </div>
         <div className="Item-text">
           <span className="highlight-1">Todo list</span>
@@ -193,7 +272,7 @@ export default function Portfolio() {
 
       <div className="Portfolio-item">
         <div className="Item-img-container">
-          <img src={item1} alt="" className="Item-img" />
+          <img src={item1} alt="item1" className="Item-img" />
         </div>
         <div className="Item-text">
           <span className="highlight-1">Online chat</span>
